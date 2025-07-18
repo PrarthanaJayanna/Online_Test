86893c236098373df59179c6e3898d6e0e9e31df
Prarthana Jayanna <prarthanajayanna@gmail.com>
2025-07-18 13:13:18 +1000

    Add Visual Studio solution, project files, and .gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BackendTestCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
