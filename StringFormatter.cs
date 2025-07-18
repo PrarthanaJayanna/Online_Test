ce4aad81c71d5ff94ad1915b226386381f938662
Prarthana Jayanna <prarthanajayanna@gmail.com>
2025-07-18 14:02:35 +1000

    return an empty string if items is null pointer or has no entry

--- StringFormatter.cs
+++ StringFormatter.cs
@@ -5,9 +5,14 @@ namespace PointsBet_Backend_Online_Code_Test
     public static class StringFormatter // static class with static method
     {
         // Returns a comma-separated string, wrapping each item in the given quote.
-        // Example: Input: ["table", "pen"] with quote ''' => Output: 'table', 'pen'
+        // Example: Input: ["table", "pen"] with quote ''' => 'table', 'pen'
         public static string ToCommaSeparatedList(string[] items, string quote)
         {
+            // checking if items is null before we check it's length using .Length
+            // since checking .Length on null will throw an exception
+            if (items == null || items.Length == 0)
+                return string.Empty;
+
             StringBuilder qry = new StringBuilder(string.Format("{0}{1}{0}", quote, items[0]));
 
             if (items.Length > 1)
