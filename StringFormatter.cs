76a4557d4ad2e6a87ef1f6b2ac4ace4711f0539f
Prarthana Jayanna <prarthanajayanna@gmail.com>
2025-07-18 14:12:14 +1000

    Refactor ToCommaSeparatedList to use LINQ for item formatting

--- StringFormatter.cs
+++ StringFormatter.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Linq;
 
 namespace PointsBet_Backend_Online_Code_Test
 {
@@ -13,17 +14,7 @@ namespace PointsBet_Backend_Online_Code_Test
             if (items == null || items.Length == 0)
                 return string.Empty;
 
-            StringBuilder qry = new StringBuilder(string.Format("{0}{1}{0}", quote, items[0]));
-
-            if (items.Length > 1)
-            {
-                for (int i = 1; i < items.Length; i++)
-                {
-                    qry.Append(string.Format(", {0}{1}{0}", quote, items[i]));
-                }
-            }
-
-            return qry.ToString();
+            return string.Join(", ", items.Select(item => $"{quote}{item}{quote}"));
         }
     }
 }
